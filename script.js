4da1fddd9ad3a3359090666ff69464b237ed1ee5
Aryan Singh Thakur <152160049+heyoaryan@users.noreply.github.com>
2024-08-23 22:18:24 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// Add event listeners to the links
+function alertMail() {
+    alert("You are about to send an email to iamaryan721@gmail.com");
+}
+
+function alertLinkedIn() {
+    alert("You are about to visit my LinkedIn profile");
+}
+
+function alertGitHub() {
+    alert("You are about to visit my GitHub profile");
+}
